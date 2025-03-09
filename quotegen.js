45156089434ccd0155e5ae99b74d71e369eebf01
thyszcl <thyszcl@gmail.com>
2025-03-09 16:50:56 +0800

    Add files via upload

---
+++ quotegen.js
@@ -0,0 +1,29 @@
+const quote = document.querySelector(".quote")
+author = document.querySelector(".name")
+quoteButton = document.querySelector(".newquote")
+speechButton = document.querySelector(".speech")
+copyButton = document.querySelector(".copy")
+synth = speechSynthesis;
+
+function quoteGen(){
+    
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+        quote.innerText = result.content;
+        author.innerText = `~ ${result.author}`;
+    });
+}
+quoteButton.addEventListener("click", quoteGen);
+
+
+speechButton.addEventListener("click", ()=>{
+    let utterance = new SpeechSynthesisUtterance(`${quote.innerText} by ${author.innerText}`);
+    synth.speak(utterance);
+    
+});
+
+
+copyButton.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quote.innerText);
+});
+
+
